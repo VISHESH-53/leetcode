26a949bf3f22f004803e7d04d4b78afb43cbeeb5
VISHESH AGRAWAL <visheshagrawal53@gmail.com>
2026-07-02 21:14:48 +0530

    Time: 11 ms (32.71%), Space: 19.4 MB (94.04%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,8 +1,18 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s="".join(filter(str.isalnum,s)).lower()
-        n=len(s)
-        for i in range(0,n//2):
-            if s[i]!=s[n-i-1]:
+
+        left, right = 0, len(s) - 1
+        
+        while left < right:
+ 
+            if not s[left].isalnum():
+                left += 1 
+            elif not s[right].isalnum():
+                right -= 1 
+            elif s[left].lower() != s[right].lower():
                 return False
+            else:
+                left += 1   
+                right -= 1
+                
         return True
