45fa62b1adeb2b52e1ba2d02ca3b3ed6603d56fa
VISHESH AGRAWAL <visheshagrawal53@gmail.com>
2026-08-21 12:00:05 +0530

    Time: 0 ms (100%), Space: 19.4 MB (60.42%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -5,12 +5,13 @@ class Solution:
         subset=[]
         def dfs(i):
             if i==len(nums):
-                if subset not in res:
-                    res.append(subset.copy())
+                res.append(subset.copy())
                 return
             subset.append(nums[i])
             dfs(i+1)
             subset.pop()
+            while i+1<len(nums) and nums[i]==nums[i+1]:
+                i+=1
             dfs(i+1)
         dfs(0)
         return res
