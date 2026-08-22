06637a7c6fa75fa61c5bf4950e8f92e616fe429f
VISHESH AGRAWAL <visheshagrawal53@gmail.com>
2026-08-22 08:03:52 +0530

    Time: 0 ms (100%), Space: 19.3 MB (24.05%) - LeetHub

--- 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.py
+++ 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.py
@@ -1,11 +1,10 @@
+import math
 class Solution:
     def checkDivisibility(self, n: int) -> bool:
         x=str(n)
-        s=0
-        p=1
-        for i in x:
-            s+=int(i)
-            p*=int(i)
+        s=sum(map(int, str(n)))
+        p=math.prod(map(int,str(n)))
+
         if n%(p+s)==0:
             return True
         else:
